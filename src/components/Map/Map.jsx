ff56669c545e82cffd10340e2718da2219f7eb27
K-Kyler <khaiquang690@gmail.com>
2021-07-09 23:16:19 +0700

    Remove sensitive

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,12 @@
 import GoogleMapReact from "google-map-react";
+import { useState } from "react";
 import { Marker } from "../Marker/Marker";
+import { MarkerInfo } from "../Marker/MarkerInfo/MarkerInfo";
 import "./Map.css";
 
 export const Map = ({ zoom, center, wildfireEvents }) => {
+  const [markerInfo, setMarkerInfo] = useState(null);
+
   return (
     <div className="map">
       <GoogleMapReact
@@ -11,13 +15,19 @@ export const Map = ({ zoom, center, wildfireEvents }) => {
         defaultZoom={zoom}
       >
         {wildfireEvents.map((event) => {
+          // Wildfire event id is 8
           if (event.categories[0].id === 8) {
-            // Wildfire event id is 8
+            const markerInfoObject = {
+              id: event.id,
+              title: event.title,
+            };
+
             return (
               <Marker
                 key={event.id}
                 lat={event.geometries[0].coordinates[1]}
                 lng={event.geometries[0].coordinates[0]}
+                clickHandler={() => setMarkerInfo(markerInfoObject)}
               />
             );
           }
@@ -25,6 +35,8 @@ export const Map = ({ zoom, center, wildfireEvents }) => {
           return null;
         })}
       </GoogleMapReact>
+
+      {markerInfo && <MarkerInfo {...markerInfo} />}
     </div>
   );
 };
--- src/components/Marker/MarkerInfo/MarkerInfo.jsx
+++ src/components/Marker/MarkerInfo/MarkerInfo.jsx
@@ -1,5 +1,11 @@
 import "./MarkerInfo.css";
 
-export const MarkerInfo = () => {
-  return <div></div>;
+export const MarkerInfo = ({ id, title }) => {
+  return (
+    <div className="markerInfo">
+      <h2>Marker Location</h2>
+      <p>ID: {id}</p>
+      <p>TITLE: {title}</p>
+    </div>
+  );
 };
